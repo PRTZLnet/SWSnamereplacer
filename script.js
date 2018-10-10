4fde94c93e6694d49eb4ec994406c7410abbd4e0
Quinn Brown <PRTZLnet@users.noreply.github.com>
2018-10-09 22:36:07 -0400

    Update script.js
    added more names

--- script.js
+++ script.js
@@ -32,12 +32,13 @@ function walk(node)
 function handleText(textNode) 
 {
 	var v = textNode.nodeValue;
-	var rel = ['Christian', 'Muslim', 'Atheist', 'Agnostic', 'Hindu', 'Sikh', 'Taoist', 'Shinto', 'Jewish', 'Buddhist'];
+	var rel = ['Christian', 'Muslim','Atheist','Agnostic','Hindu','Sikh','Taoist','Shinto','Jewish','Buddhist','Mithraic','Manichean','Cathar','Druidic','Shinto','Hussite','Waldensian','Taoist','Confucian','Pagan','Zoroastrian','Yazidi','Nestorian','Asatru','Messalian','Tengri','Wiccan','Lollard','Bogomilist','Mazdakian','Catholic','Orthodox','Shia','Sunni','Ibadi','Jainist','Coptic','Monophysite','Romuva','Suomenusko', 'Zunist', 'Kharijite'];
 	var rel_gen = rel[Math.floor(Math.random()*rel.length)];
-	var town = ['Hamburg', 'Munich', 'Frankfurt', 'Cologne', 'Dresden', 'Berlin', 'Nuremberg', 'Hanover', 'Munich', 'Leipzig', 'Stuggart'];
+	var town = ['Hamburg','Munich','Berlin','Hanover','Munich','Stuggart','Magdeburg','Mainz','Cologne','Dresden','Stuttgart','Hanover','Wilhelmshaven','Jena','Hamburg','Leipzig','Frankfurt','Aachen','Potsdam','Heidelberg','Würzburg','Nuremburg','Freiburg','Trier','Königsberg','Danzig','Dortmund','Bremen','Tannenburg','Lubeck','Kiel','Rostock','Augsberg','Auerstedt','Weimar','Ulm','Bonn','Brunswick'];
 	var town_gen = town[Math.floor(Math.random()*town.length)];
   
 	v = v.replace(/\bChristianMunich\b/g, rel_gen+town_gen);
 	v = v.replace(/bchristianmunich\b/g, rel_gen+town_gen);
 	textNode.nodeValue = v;
 }
+
